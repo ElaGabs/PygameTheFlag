75cc24fa14e87237b3e4109c7c7e15e78efec948
ElaGabs <22elag@github.com>
2023-09-19 11:18:40 +0300

    Screen coloring DONE

--- screen.py
+++ screen.py
@@ -1,37 +1,19 @@
 import pygame
 import consts
-from game_field import grid
 pygame.init()
 
-screen = pygame.display.set_mode([800,600])
 pygame.display.set_caption("The Flag")
+screen = pygame.display.set_mode([900, 500])
+background = consts.GREEN
+screen.fill(background)
 
-background_color = consts.GREEN
+running = True
 
-bush = pygame.image.load("grass.png")
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((background))
+    pygame.display.flip()
+    pass
 
-# we use the sizes to draw as well as to do our "steps" in the loops.
-grid_node_width = 10
-grid_node_height = 10
-game_field = grid()
-
-def createSquare(x, y, color):
-    pygame.draw.rect(screen, consts.GREEN, [x, y, grid_node_width, grid_node_height])
-
-def visualizeGrid():
-    y = 0  # we start at the top of the screen
-    for row in game_field:
-        x = 0 # for every row we start at the left of the screen again
-        for item in row:
-            if item == 0:
-                createSquare(x, y, (255, 255, 255))
-            else:
-                createSquare(x, y, (0, 0, 0))
-
-            x += grid_node_width # for every item/number in that row we move one "step" to the right
-        y += grid_node_height   # for every new row we move one "step" downwards
-    pygame.display.update()
-
-visualizeGrid()  # call the function
-while True:
-    pass  # keeps the window open so you can see the result.
