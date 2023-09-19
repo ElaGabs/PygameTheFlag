d01e63c9bf44f8a3c09f6f3f491b6f2ff4aafc61
work <adiissan415@gmail.com>
2023-09-19 10:39:51 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,5 +1,6 @@
 import pygame
 import consts
+from game_field import grid
 pygame.init()
 
 screen = pygame.display.set_mode([800,600])
@@ -9,30 +10,23 @@ background_color = consts.GREEN
 
 bush = pygame.image.load("grass.png")
 
-
-
-
 # we use the sizes to draw as well as to do our "steps" in the loops.
 grid_node_width = 10
 grid_node_height = 10
+game_field = grid()
 
 def createSquare(x, y, color):
-    pygame.draw.rect(gridDisplay, color, [x, y, grid_node_width, grid_node_height ])
+    pygame.draw.rect(screen, consts.GREEN, [x, y, grid_node_width, grid_node_height])
 
 def visualizeGrid():
     y = 0  # we start at the top of the screen
-    for row in matrix:
+    for row in game_field:
         x = 0 # for every row we start at the left of the screen again
         for item in row:
-            if item == 0:
-                createSquare(x, y, (255, 255, 255))
-            else:
-                createSquare(x, y, (0, 0, 0))
-
-            x += grid_node_width # for ever item/number in that row we move one "step" to the right
+            createSquare(x, y, consts.GREEN)
+            x += grid_node_width # for every item/number in that row we move one "step" to the right
         y += grid_node_height   # for every new row we move one "step" downwards
     pygame.display.update()
-
 
 visualizeGrid()  # call the function
 while True:
